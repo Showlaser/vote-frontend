3c61c31a216c0ab49e80c954d3b3924d1a76a5ea
Vincent Darwinkel <vincent.darwinkel@hotmail.nl>
2022-03-30 11:03:37 +0200

    Fixed bug that allowed user to vote multiple times

--- src/components/shared/modal/index.jsx
+++ src/components/shared/modal/index.jsx
@@ -1,20 +1,32 @@
 import { Button } from "@mui/material";
+import { useState } from "react";
 import "./index.css";
 
 export default function Modal({ modal }) {
+  const [userHasClicked, setUserHasClicked] = useState(false);
+
   return modal?.show ? (
     <div id="modal">
       <div id="modal-items">
         <h2>{modal?.title}</h2>
         <p>{modal?.description}</p>
         <Button
+          disabled={userHasClicked}
           variant="contained"
           color="primary"
-          onClick={() => modal?.onOkClick()}
+          onClick={() => {
+            setUserHasClicked(true);
+            modal?.onOkClick();
+          }}
         >
           Vote
         </Button>
-        <Button onClick={() => modal?.onCancelClick()}>Cancel</Button>
+        <Button
+          disabled={userHasClicked}
+          onClick={() => modal?.onCancelClick()}
+        >
+          Cancel
+        </Button>
       </div>
     </div>
   ) : null;
